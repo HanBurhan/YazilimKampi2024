1b181c60eb4500e458599d388a5fba252d9d5e1c
Burhan <83255703+HanBurhan@users.noreply.github.com>
2024-03-04 21:05:10 +0300

    1.Hafta ilk upload

---
+++ Intro/Intro/Program.cs
@@ -0,0 +1,64 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Intro.Business;
+using Intro.Entities;
+
+Console.WriteLine("Hello, World!");
+
+string message1 = "Krediler";
+int term = 12;
+double amount = 100000;
+
+//Variables --> camelCase
+bool isAuthenticated = true;
+Console.WriteLine(message1);
+
+//condition
+if (isAuthenticated)
+{
+    Console.WriteLine("Button-->Hosşgeldin Burhan.");
+}
+else
+{
+    Console.WriteLine("Button-->Sisteme giriş yap.");
+}
+
+string[] loans = { "Kredi 1", "Kredi 2", "Kredi 3" };
+
+for (int i = 0; i < loans.Length; i++)
+{
+    Console.WriteLine(loans[i]);
+}
+
+Course course1 = new Course();
+course1.Id = 1;
+course1.Name = "C#";
+course1.Description = ".NET 8";
+course1.Price = 0;
+
+Course course2 = new Course();
+course2.Id = 2;
+course2.Name = "JAVA";
+course2.Description = "Jva 17";
+course2.Price = 20;
+
+Course course3 = new Course();
+course3.Id = 3;
+course3.Name = "Python";
+course3.Description = "Python 3.12";
+course3.Price = 0;
+
+Course[] courses = { course1, course2, course3 };
+
+for (int i = 0; i < courses.Length; i++)
+{
+    Console.WriteLine(courses[i].Name + " > " + courses[i].Price + "TL.");
+}
+
+CourseManager courseManager = new();
+Course[] courses2 = courseManager.GetAll();
+for (int i = 0; i < courses2.Length; i++)
+{
+    Console.WriteLine(courses2[i].Name + " > " + courses2[i].Price + "TL.");
+}
+
+Console.WriteLine("Kod bitti!");
